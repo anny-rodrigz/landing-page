7d105d2d3bf553177322920cede1afdf87d1832e
anny-rodrigz <biamartinsfxfx@gmail.com>
2025-04-04 17:09:05 -0300

    Arquivos iníciais

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Contacts from "./components/Contacts";
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+
+export default function Home() {
+  return (
+    <main>
+      <Hero />
+      <Skills />
+      <Projects />
+      <Contacts />
+    </main>
+  );
+}
